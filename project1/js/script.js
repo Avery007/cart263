47b00cb6a8dc5b78537cdb59b17c531405bf54a9
Avery <veleda94@hotmail.com>
2020-02-04 14:56:03 -0500

    P1: Finish the keycontrol of images
    shift the gif and static image by using key

--- project1/js/script.js
+++ project1/js/script.js
@@ -9,3 +9,61 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+let $sisyphus=$("#sisyphus");
+let $static=$("#static");
+
+ $(document).ready(setup);
+
+function setup(){
+setTimeout(shift,1000);
+document.addEventListener('keydown', movement);
+document.addEventListener('keyup', noMove);
+}
+
+function movement(e){
+if(e.keyCode===39){
+ $("#static").css('display','none');
+
+
+}
+}
+
+function noMove(){
+$("#static").css('display','block');
+
+
+}
+
+
+
+function shift(){
+
+//  $static.css('width','+=500');
+$('#text').on('mouseover', dialog );
+//$('#text').on('mouseover', dialog );
+ //dialog();
+}
+function dialog(){
+
+  $("#dialog").dialog({
+  dialogClass: "no-close",
+//  position: { my:200, at: 300, };
+
+    height: 400,
+  buttons: [
+    {
+      text: "OK",
+      icon: "ui-icon-heart",
+
+      click: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  ]
+});
+}
+
+function changeColor(){
+$(this).css('color','red');
+}
