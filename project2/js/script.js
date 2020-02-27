377473e8e1ed95dd348513d09a91fff009742dd5
Avery <veleda94@hotmail.com>
2020-02-26 20:22:43 -0500

    P2:Added function to allow players to adjust the color and size of text

--- project2/js/script.js
+++ project2/js/script.js
@@ -9,19 +9,40 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
-
+let size=10;
 $(document).ready(setup);
+
 
 
 function input() {
    $("#myInput").show();
   $("#buttonClose").show();
-alert("hello");
+
 }
 
 function generate(){
   $("#buttonClose").hide();
   var str = $("#myInput").val();
   $('#text').text(str);
+
+}
+function colorChange(){
+
+ let r=Math.floor(Math.random()*255);
+ let g=Math.floor(Math.random()*255);
+ let b=Math.floor(Math.random()*255);
+ //Change the background color of the element to random color
+  document.getElementById("text").style.color = `rgb(${r}, ${g}, ${b})`;
+
+}
+
+function sizeChange(){
+
+
+ size = size+5;
+document.getElementById("text").style.fontSize = `${size}px`;
+console.log(size);
+ //Change the background color of the element to random color
+//theSize.style.fontSize='25px';
 
 }
