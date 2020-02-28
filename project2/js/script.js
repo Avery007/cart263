a0fe47ecde191267c2fc8431e44623e2de5e230f
Avery <veleda94@hotmail.com>
2020-02-28 00:04:16 -0500

    P2: Added function to rotate images while clicking

--- project2/js/script.js
+++ project2/js/script.js
@@ -9,19 +9,45 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
-let size=10;
-let positionX=500;
-let positionY=100;
+let size = 10;
+let positionX = 500;
+let positionY = 100;
+let imgWidth = 200;
+let drag = true;
+let rotation = 0;
+let opacity=10;
+
 
 $(document).ready(setup);
 
-function chosing(){
-$('.politicImg').show();
-$('#trump').draggable();
-$('#globe').draggable();
+function chosing() {
+  $('#trump').show();
+  $('#globe').show();
+
+  $('#trump').draggable();
+//$('#trump').on('click', rotate);
+
+document.getElementById('trump').addEventListener('click', rotate);
+document.getElementById('globe').addEventListener('click', rotate);
+  $('#globe').draggable();
+
 
 }
-function setup(){
+
+function rotate(e) {
+
+  rotation = rotation + 5;
+  e.target.style.transform = `rotate(${rotation}deg)`;
+
+}
+//function opacityChange() {
+
+  //opacity = opacity-1;
+
+//document.getElementById("trump").style.opacity= `${opacity}`;
+
+//}
+function setup() {
   document.addEventListener('keydown', moveUp);
   document.addEventListener('keydown', moveDown);
   document.addEventListener('keydown', moveLeft);
@@ -30,71 +56,74 @@ function setup(){
 
 }
 
-function moveUp(e){
-  if(e.keyCode===87)
-  {  positionY=positionY-10;
-document.getElementById("text").style.top = `${positionY}px`;
+function moveUp(e) {
+  if (e.keyCode === 87) {
+    positionY = positionY - 10;
+    document.getElementById("text").style.top = `${positionY}px`;
+
+  }
 
 }
 
-}
-function moveDown(e){
-  if(e.keyCode===83)
-  {  positionY=positionY+10;
-document.getElementById("text").style.top = `${positionY}px`;
+function moveDown(e) {
+  if (e.keyCode === 83) {
+    positionY = positionY + 10;
+    document.getElementById("text").style.top = `${positionY}px`;
+
+  }
 
 }
 
-}
+function moveLeft(e) {
+  if (e.keyCode === 65) {
+    positionX = positionX - 10;
+    document.getElementById("text").style.left = `${positionX}px`;
 
-function moveLeft(e){
-  if(e.keyCode===65)
-  {  positionX=positionX-10;
-document.getElementById("text").style.left= `${positionX}px`;
-
-}
+  }
 
 }
-function moveRight(e){
-  if(e.keyCode===68)
-  {  positionX=positionX+10;
-document.getElementById("text").style.left= `${positionX}px`;
 
-}
+function moveRight(e) {
+  if (e.keyCode === 68) {
+    positionX = positionX + 10;
+    document.getElementById("text").style.left = `${positionX}px`;
+
+  }
 
 
 }
 
 function input() {
-   $("#myInput").show();
+  $("#myInput").show();
   $("#buttonClose").show();
 
 }
 
-function generate(){
+function generate() {
   $("#buttonClose").hide();
   var str = $("#myInput").val();
   $('#text').text(str);
 
 }
-function colorChange(){
 
- let r=Math.floor(Math.random()*255);
- let g=Math.floor(Math.random()*255);
- let b=Math.floor(Math.random()*255);
- //Change the background color of the element to random color
+function colorChange() {
+
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  //Change the background color of the element to random color
   document.getElementById("text").style.color = `rgb(${r}, ${g}, ${b})`;
 
 }
 
 
-function sizeChange(){
+function sizeChange() {
 
 
- size = size+5;
-document.getElementById("text").style.fontSize = `${size}px`;
-console.log(size);
- //Change the background color of the element to random color
-//theSize.style.fontSize='25px';
+  size = size + 5;
+  document.getElementById("text").style.fontSize = `${size}px`;
+  console.log(size);
+  //Change the background color of the element to random color
+  //theSize.style.fontSize='25px';
 
 }
