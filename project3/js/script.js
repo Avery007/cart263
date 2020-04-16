78aeb75e8f9fc27df0fef625067c27055a357231
Avery <veleda94@hotmail.com>
2020-04-16 13:47:52 -0400

    P3: Change display of the project

--- project3/js/script.js
+++ project3/js/script.js
@@ -9,7 +9,9 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
-
+let voice1="sellphone is the best entertainment";
+let voice2="sellphone is the best beauty";
+let voice3="we cannot live without sellphone";
 $(document).ready(setup);
 
 
@@ -20,6 +22,36 @@ function setup() {
     volume: 10,
 
   });
+
+
+enterScreen($('#screen2'),voice2);
+enterScreen($('#screen3'),voice3);
+enterScreen($('#screen4'),voice4);
+}
+
+function enterScreen(screen,voice){
+screen.mouseover(
+  function() {screen.css('width','95vw');
+              screen.css('height','90vh');
+            screen.css('zIndex','2');
+             responsiveVoice.speak(voice, 'UK English Male', {
+                pitch: 5,
+                rate: 1,
+                volume: 10,
+
+              });
+
+
+});
+
+screen.mouseleave(
+  function() {screen.css('width','10vw');
+              screen.css('height','10vh');
+              screen.css('zIndex','1');
+
+
+
+});
 
 
 }
